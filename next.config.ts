c884878bf9b2a51e395504987159e3732d8b0cf1
Lance Red <daniellancered@gmail.com>
2026-08-18 21:12:17 +0800

    disable dev indicators

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  devIndicators: {
+    buildActivity: false,
+    appIsrStatus: false,
+  },
+};
 
 export default nextConfig;
